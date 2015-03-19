394b56a7fd09d479359bbd7e644aa5a4a60da5ad
Alex Ames <amablue@google.com>
2015-03-19 11:10:11 -0700

    Added a pindrop demo project.
    Added a demo project that uses SDL to create a window where you can
    place sounds and listeners and see how they interact.
    
    Added json files that drive the demo project.
    
    The sound assets were taken from Pie Noon.
    
    Updated thee build_assets script to correctly build the json assets.
    
    Built and tested on Linux.
    
    Bug: 18809549
    Change-Id: Ia5fe1eba4c12b4268284c0b55eea90a9cd044778

--- scripts/build_assets.py
+++ scripts/build_assets.py
@@ -16,15 +16,16 @@
 """Builds all assets under src/rawassets/, writing the results to assets/.
 
 Finds the flatbuffer compiler then uses it to convert the JSON files to
-flatbuffer binary files so that they can be loaded by the engine.  If you would
-like to clean all generated files, you can call this script with the argument
-'clean'.
+flatbuffer binary files.  If you would like to clean all generated files, you
+can call this script with the argument 'clean'.
 """
 
+import argparse
 import distutils.spawn
 import glob
 import os
 import platform
+import shutil
 import subprocess
 import sys
 
@@ -38,12 +39,16 @@ PREBUILTS_ROOT = os.path.abspath(os.path.join(os.path.join(PROJECT_ROOT),
                                               os.path.pardir, os.path.pardir,
                                               'prebuilts'))
 
+PINDROP_ROOT = os.path.abspath(os.path.join(os.path.join(PROJECT_ROOT),
+                                            os.path.pardir, os.path.pardir,
+                                            'libs', "audio_engine"))
+
 # Directories that may contains the FlatBuffers compiler.
 FLATBUFFERS_PATHS = [
     os.path.join(PROJECT_ROOT, 'bin'),
     os.path.join(PROJECT_ROOT, 'bin', 'Release'),
-    os.path.join(PROJECT_ROOT, 'bin', 'Debug'),
-]
+    os.path.join(PROJECT_ROOT, 'bin', 'Debug')
+    ]
 
 # Directory to place processed assets.
 ASSETS_PATH = os.path.join(PROJECT_ROOT, 'assets')
@@ -51,20 +56,14 @@ ASSETS_PATH = os.path.join(PROJECT_ROOT, 'assets')
 # Directory where unprocessed assets can be found.
 RAW_ASSETS_PATH = os.path.join(PROJECT_ROOT, 'src', 'rawassets')
 
-# Directory where processed sound flatbuffer data can be found.
-SOUND_PATH = os.path.join(ASSETS_PATH, 'sounds')
-
 # Directory where unprocessed sound flatbuffer data can be found.
 RAW_SOUND_PATH = os.path.join(RAW_ASSETS_PATH, 'sounds')
-
-# Directory where processed sound flatbuffer data can be found.
-SOUND_BANK_PATH = os.path.join(ASSETS_PATH, 'sound_banks')
 
 # Directory where unprocessed sound flatbuffer data can be found.
 RAW_SOUND_BANK_PATH = os.path.join(RAW_ASSETS_PATH, 'sound_banks')
 
 # Directory where unprocessed assets can be found.
-SCHEMA_PATH = os.path.join(PROJECT_ROOT, 'src', 'flatbufferschemas')
+SCHEMA_PATHS = [ os.path.join(PINDROP_ROOT, 'schemas') ]
 
 # Windows uses the .exe extension on executables.
 EXECUTABLE_EXTENSION = '.exe' if platform.system() == 'Windows' else ''
@@ -73,51 +72,21 @@ EXECUTABLE_EXTENSION = '.exe' if platform.system() == 'Windows' else ''
 FLATC_EXECUTABLE_NAME = 'flatc' + EXECUTABLE_EXTENSION
 
 
-def processed_json_dir(path):
-  """Take the path to a raw json asset and convert it to target directory."""
-  return os.path.dirname(path.replace(RAW_ASSETS_PATH, ASSETS_PATH))
-
-
 class FlatbuffersConversionData(object):
   """Holds data needed to convert a set of json files to flatbuffer binaries.
 
   Attributes:
     schema: The path to the flatbuffer schema file.
     input_files: A list of input files to convert.
-    output_path: The path to the output directory where the converted files will
-        be placed.
   """
 
-  def __init__(self, schema, input_files, output_path):
-    """Initializes this object's schema, input_files and output_path."""
+  def __init__(self, schema, input_files):
+    """Initializes this object's schema and input_files."""
     self.schema = schema
     self.input_files = input_files
-    self.output_path = output_path
 
 
-# A list of json files and their schemas that will be converted to binary files
-# by the flatbuffer compiler.
-FLATBUFFERS_CONVERSION_DATA = [
-    FlatbuffersConversionData(
-        schema=os.path.join(SCHEMA_PATH, 'audio_config.fbs'),
-        input_files=[os.path.join(RAW_ASSETS_PATH, 'audio_config.json')],
-        output_path=ASSETS_PATH),
-    FlatbuffersConversionData(
-        schema=os.path.join(SCHEMA_PATH, 'buses.fbs'),
-        input_files=[os.path.join(RAW_ASSETS_PATH, 'buses.json')],
-        output_path=ASSETS_PATH),
-    FlatbuffersConversionData(
-        schema=os.path.join(SCHEMA_PATH, 'sound_bank_def.fbs'),
-        input_files=glob.glob(os.path.join(RAW_SOUND_BANK_PATH, '*.json')),
-        output_path=SOUND_BANK_PATH),
-    FlatbuffersConversionData(
-        schema=os.path.join(SCHEMA_PATH, 'sound_collection_def.fbs'),
-        input_files=glob.glob(os.path.join(RAW_SOUND_PATH, '*.json')),
-        output_path=SOUND_PATH),
-]
-
-
-def find_executable(name, paths):
+def find_in_paths(name, paths):
   """Searches for a file with named `name` in the given paths and returns it."""
   for path in paths:
     full_path = os.path.join(path, name)
@@ -127,30 +96,53 @@ def find_executable(name, paths):
   return name
 
 
+# A list of json files and their schemas that will be converted to binary files
+# by the flatbuffer compiler.
+FLATBUFFERS_CONVERSION_DATA = [
+    FlatbuffersConversionData(
+        schema=find_in_paths('audio_config.fbs', SCHEMA_PATHS),
+        input_files=[os.path.join(RAW_ASSETS_PATH, 'audio_config.json')]),
+    FlatbuffersConversionData(
+        schema=find_in_paths('buses.fbs', SCHEMA_PATHS),
+        input_files=[os.path.join(RAW_ASSETS_PATH, 'buses.json')]),
+    FlatbuffersConversionData(
+        schema=find_in_paths('sound_bank_def.fbs', SCHEMA_PATHS),
+        input_files=glob.glob(os.path.join(RAW_SOUND_BANK_PATH, '*.json'))),
+    FlatbuffersConversionData(
+        schema=find_in_paths('sound_collection_def.fbs', SCHEMA_PATHS),
+        input_files=glob.glob(os.path.join(RAW_SOUND_PATH, '*.json')))
+    ]
+
+
 # Location of FlatBuffers compiler.
-FLATC = find_executable(FLATC_EXECUTABLE_NAME, FLATBUFFERS_PATHS)
+FLATC = find_in_paths(FLATC_EXECUTABLE_NAME, FLATBUFFERS_PATHS)
 
 
 class BuildError(Exception):
   """Error indicating there was a problem building assets."""
 
-  def __init__(self, argv, error_code):
+  def __init__(self, argv, error_code, message=None):
     Exception.__init__(self)
     self.argv = argv
     self.error_code = error_code
+    self.message = message if message else ''
 
 
 def run_subprocess(argv):
-  process = subprocess.Popen(argv)
+  try:
+    process = subprocess.Popen(argv)
+  except OSError as e:
+    raise BuildError(argv, 1, message=str(e))
   process.wait()
   if process.returncode:
     raise BuildError(argv, process.returncode)
 
 
-def convert_json_to_flatbuffer_binary(json, schema, out_dir):
+def convert_json_to_flatbuffer_binary(flatc, json, schema, out_dir):
   """Run the flatbuffer compiler on the given json file and schema.
 
   Args:
+    flatc: Path to the flatc binary.
     json: The path to the json file to convert to a flatbuffer binary.
     schema: The path to the schema to use in the conversion process.
     out_dir: The directory to write the flatbuffer binary.
@@ -158,7 +150,10 @@ def convert_json_to_flatbuffer_binary(json, schema, out_dir):
   Raises:
     BuildError: Process return code was nonzero.
   """
-  command = [FLATC, '-o', out_dir, '-b', schema, json]
+  command = [flatc, '-o', out_dir]
+  for path in SCHEMA_PATHS:
+    command.extend(['-I', path])
+  command.extend(['-b', schema, json])
   run_subprocess(command)
 
 
@@ -170,30 +165,79 @@ def needs_rebuild(source, target):
     target: The target file which we may need to rebuild.
 
   Returns:
-    True if the source file is newer than the target, or if the target file does
-    not exist.
+    True if the source file is newer than the target, or if the target file
+    does not exist.
   """
   return not os.path.isfile(target) or (
       os.path.getmtime(source) > os.path.getmtime(target))
 
 
-def processed_json_path(path):
-  """Take the path to a raw json asset and convert it to target bin path."""
-  return path.replace(RAW_ASSETS_PATH, ASSETS_PATH).replace('.json', '.bin')
+def processed_json_path(path, target_directory):
+  """Take the path to a raw json asset and convert it to target bin path.
+
+  Args:
+    target_directory: Path to the target assets directory.
+  """
+  return path.replace(RAW_ASSETS_PATH, target_directory).replace(
+      '.json', '.bin')
 
 
-def generate_flatbuffer_binaries():
-  """Run the flatbuffer compiler on the all of the flatbuffer json files."""
+def generate_flatbuffer_binaries(flatc, target_directory):
+  """Run the flatbuffer compiler on the all of the flatbuffer json files.
+
+  Args:
+    flatc: Path to the flatc binary.
+    target_directory: Path to the target assets directory.
+  """
   for element in FLATBUFFERS_CONVERSION_DATA:
     schema = element.schema
-    output_path = element.output_path
-    if not os.path.exists(output_path):
-      os.makedirs(output_path)
     for json in element.input_files:
-      target = processed_json_path(json)
+      target = processed_json_path(json, target_directory)
+      target_file_dir = os.path.dirname(target)
+      if not os.path.exists(target_file_dir):
+        os.makedirs(target_file_dir)
       if needs_rebuild(json, target) or needs_rebuild(schema, target):
-        convert_json_to_flatbuffer_binary(
-            json, schema, output_path)
+        convert_json_to_flatbuffer_binary(flatc, json, schema, target_file_dir)
+
+
+def copy_assets(target_directory):
+  """Copy modified assets to the target assets directory.
+
+  All files are copied from ASSETS_PATH to the specified target_directory if
+  they're newer than the destination files.
+
+  Args:
+    target_directory: Directory to copy assets to.
+  """
+  assets_dir = target_directory
+  source_dir = os.path.realpath(ASSETS_PATH)
+  if source_dir != os.path.realpath(assets_dir):
+    for dirpath, _, files in os.walk(source_dir):
+      for name in files:
+        source_filename = os.path.join(dirpath, name)
+        relative_source_dir = os.path.relpath(source_filename, source_dir)
+        target_dir = os.path.dirname(os.path.join(assets_dir,
+                                                  relative_source_dir))
+        target_filename = os.path.join(target_dir, name)
+        if not os.path.exists(target_dir):
+          os.makedirs(target_dir)
+        if (not os.path.exists(target_filename) or
+            (os.path.getmtime(target_filename) <
+             os.path.getmtime(source_filename))):
+              shutil.copy2(source_filename, target_filename)
+
+
+def clean_flatbuffer_binaries(target_directory):
+  """Delete all the processed flatbuffer binaries.
+
+  Args:
+    target_directory: Path to the target assets directory.
+  """
+  for element in FLATBUFFERS_CONVERSION_DATA:
+    for json in element.input_files:
+      path = processed_json_path(json, target_directory)
+      if os.path.isfile(path):
+        os.remove(path)
 
 
 def clean():
@@ -203,41 +247,40 @@ def clean():
 
 def handle_build_error(error):
   """Prints an error message to stderr for BuildErrors."""
-  sys.stderr.write('Error running command `%s`. Returned %s.\n' % (
-      ' '.join(error.argv), str(error.error_code)))
+  sys.stderr.write('Error running command `%s`. Returned %s.\n%s\n' % (
+      ' '.join(error.argv), str(error.error_code), str(error.message)))
 
 
 def main(argv):
   """Builds or cleans the assets needed for the game.
 
-  To build all assets, either call this script without any arguments. Or
-  alternatively, call it with the argument 'all'. To just convert the flatbuffer
-  json files, call it with 'flatbuffers'.  To clean all converted files, call it
-  with 'clean'.
-
-  Args:
-    argv: The command line argument containing which command to run.
+  This script will build all the assets in src/rawassets.  To clean all
+  converted files, call it with 'clean'.
 
   Returns:
     Returns 0 on success.
   """
-  target = argv[1] if len(argv) >= 2 else 'all'
-  if target not in ('all', 'flatbuffers', 'clean'):
-    sys.stderr.write('No rule to build target %s.\n' % target)
-
-  if target in ('all', 'flatbuffers'):
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--flatc', default=FLATC,
+                      help='Location of the flatbuffers compiler.')
+  parser.add_argument('--output', default=ASSETS_PATH,
+                      help='Assets output directory.')
+  parser.add_argument('args', nargs=argparse.REMAINDER)
+  args = parser.parse_args()
+  target = args.args[1] if len(args.args) >= 2 else 'all'
+  if target != 'clean':
+    copy_assets(args.output)
     try:
-      generate_flatbuffer_binaries()
+      generate_flatbuffer_binaries(args.flatc, args.output)
     except BuildError as error:
       handle_build_error(error)
       return 1
-  if target == 'clean':
+  else:
     try:
       clean()
     except OSError as error:
       sys.stderr.write('Error cleaning: %s' % str(error))
       return 1
-
   return 0
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,346 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <iostream>
-
+#include <algorithm>
+#include <cstdio>
+#include <fstream>
+#include <string>
+#include "SDL.h"
+#include "audio_config_generated.h"
+#include "mathfu/constants.h"
+#include "mathfu/vector_2.h"
+#include "mathfu/vector_3.h"
 #include "pindrop/audio_engine.h"
+
+const int kScreenWidth = 640;
+const int kScreenHeight = 480;
+const float kFramesPerSecond = 60.0f;
+const Uint32 kDelayMilliseconds =
+    static_cast<Uint32>(1000.0f * 1.0f / kFramesPerSecond);
+
+const char* kWindowTitle = "Pindrop Demo";
+const char* kAudioConfig = "assets/audio_config.bin";
+const char* kSoundBank = "assets/sound_banks/my_sound_bank.bin";
+const char* kInstructionsTexture = "assets/textures/instructions.bmp";
+const char* kChannelTexture = "assets/textures/channel.bmp";
+const char* kListenerTexture = "assets/textures/listener.bmp";
+const char* kSoundHandleName = "my_sounds";
+
+typedef mathfu::Vector<float, 2> Vector2f;
+
+struct IconState {
+  IconState() : location(mathfu::kZeros2f), velocity(mathfu::kZeros2f) {}
+  Vector2f location;
+  Vector2f velocity;
+};
+
+struct ListenerIcon : public IconState {
+  ListenerIcon() : listener(nullptr) {}
+  pindrop::Listener listener;
+};
+
+struct ChannelIcon : public IconState {
+  ChannelIcon() : channel(nullptr) {}
+  pindrop::Channel channel;
+};
+
+class DemoState {
+ public:
+  DemoState()
+      : quit_(false),
+        audio_config_source_(),
+        audio_engine_(),
+        window_(nullptr),
+        renderer_(nullptr),
+        sound_handle_(nullptr),
+        instructions_texture_(nullptr),
+        channel_icons_(),
+        channel_texture_(nullptr),
+        new_channel_location_(),
+        listener_icons_(),
+        listener_texture_(nullptr),
+        new_listener_location_() {}
+
+  ~DemoState();
+
+  // Initialize the demo.
+  bool Initialize();
+
+  // Run the main loop.
+  void Run();
+
+ private:
+  SDL_Texture* LoadTexture(const char* texture_path);
+  void AdvanceFrame(float delta_time);
+  void HandleInput();
+  void UpdateIconState(IconState* icon_state, float delta_time);
+  void UpdateIcons(float delta_time);
+  void DrawInstructions();
+  void DrawIcon(const IconState& icon_state, SDL_Texture* texture);
+  void DrawIcons();
+
+  bool quit_;
+  std::string audio_config_source_;
+  pindrop::AudioEngine audio_engine_;
+  SDL_Window* window_;
+  SDL_Renderer* renderer_;
+  pindrop::SoundHandle sound_handle_;
+  SDL_Texture* instructions_texture_;
+
+  std::vector<ChannelIcon> channel_icons_;
+  SDL_Texture* channel_texture_;
+  Vector2f new_channel_location_;
+
+  std::vector<ListenerIcon> listener_icons_;
+  SDL_Texture* listener_texture_;
+  Vector2f new_listener_location_;
+};
+
+DemoState::~DemoState() {
+  SDL_DestroyTexture(instructions_texture_);
+  SDL_DestroyTexture(channel_texture_);
+  SDL_DestroyTexture(listener_texture_);
+  SDL_DestroyRenderer(renderer_);
+  SDL_DestroyWindow(window_);
+  SDL_Quit();
+}
+
+SDL_Texture* DemoState::LoadTexture(const char* texture_path) {
+  SDL_Surface* surface = SDL_LoadBMP(texture_path);
+  if (surface == nullptr) {
+    fprintf(stderr, "Could not load `%s`: %s\n", texture_path, SDL_GetError());
+    return nullptr;
+  }
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer_, surface);
+  if (texture == nullptr) {
+    fprintf(stderr, "Could not load `%s`: %s\n", texture_path, SDL_GetError());
+    SDL_DestroyTexture(texture);
+    return nullptr;
+  }
+  SDL_FreeSurface(surface);
+  return texture;
+}
+
+bool DemoState::Initialize() {
+  // Initialize SDL.
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
+    return false;
+  }
+
+  // Initialize the window.
+  window_ = SDL_CreateWindow(kWindowTitle, SDL_WINDOWPOS_UNDEFINED,
+                             SDL_WINDOWPOS_UNDEFINED, kScreenWidth,
+                             kScreenHeight, SDL_WINDOW_SHOWN);
+  if (window_ == nullptr) {
+    fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
+    return false;
+  }
+
+  // Initialize the renderer.
+  renderer_ = SDL_CreateRenderer(window_, -1, SDL_RENDERER_ACCELERATED);
+  if (renderer_ == nullptr) {
+    fprintf(stderr, "Could not create renderer: %s\n", SDL_GetError());
+    return false;
+  }
+  SDL_SetRenderDrawColor(renderer_, 255, 255, 255, 255);
+
+  // Initialize images.
+  if ((channel_texture_ = LoadTexture(kChannelTexture)) == nullptr ||
+      (listener_texture_ = LoadTexture(kListenerTexture)) == nullptr ||
+      (instructions_texture_ = LoadTexture(kInstructionsTexture)) == nullptr) {
+    return false;
+  }
+
+  // Initialize Pindrop.
+  if (!audio_engine_.Initialize(kAudioConfig) ||
+      !audio_engine_.LoadSoundBank(kSoundBank)) {
+    return false;
+  }
+
+  // Cache the SoundHandle to the sound we want to play.
+  sound_handle_ = audio_engine_.GetSoundHandle(kSoundHandleName);
+  if (sound_handle_ == nullptr) {
+    fprintf(stderr, "Could not find sound handle %s\n", kSoundHandleName);
+    return false;
+  }
+
+  // Success!
+  return true;
+}
+
+void DemoState::UpdateIconState(IconState* icon_state, float delta_time) {
+  icon_state->location += icon_state->velocity * delta_time;
+  if (icon_state->location.x() < 0) {
+    icon_state->location.x() *= -1;
+    icon_state->velocity.x() *= -1;
+  } else if (icon_state->location[0] > kScreenWidth) {
+    icon_state->location.x() -= icon_state->location[0] - kScreenWidth;
+    icon_state->velocity.x() *= -1;
+  }
+  if (icon_state->location.y() < 0) {
+    icon_state->location.y() *= -1;
+    icon_state->velocity.y() *= -1;
+  } else if (icon_state->location[1] > kScreenHeight) {
+    icon_state->location.y() -= icon_state->location[1] - kScreenHeight;
+    icon_state->velocity.y() *= -1;
+  }
+}
+
+void DemoState::UpdateIcons(float delta_time) {
+  for (size_t i = 0; i < channel_icons_.size(); ++i) {
+    ChannelIcon& icon = channel_icons_[i];
+    UpdateIconState(&icon, delta_time);
+    icon.channel.set_location(mathfu::Vector<float, 3>(icon.location, 0.0f));
+  }
+  for (size_t i = 0; i < listener_icons_.size(); ++i) {
+    ListenerIcon& icon = listener_icons_[i];
+    UpdateIconState(&icon, delta_time);
+    icon.listener.set_location(mathfu::Vector<float, 3>(icon.location, 0.0f));
+  }
+}
+
+void TextureRect(SDL_Rect* rect, const Vector2f& location,
+                 SDL_Texture* texture) {
+  SDL_QueryTexture(texture, nullptr, nullptr, &rect->w, &rect->h);
+  rect->x = location.x() - rect->w / 2;
+  rect->y = location.y() - rect->h / 2;
+}
+
+void DemoState::DrawIcon(const IconState& icon_state, SDL_Texture* texture) {
+  SDL_Rect rect = { 0, 0, 0, 0 };
+  TextureRect(&rect, icon_state.location, texture);
+  SDL_RenderCopy(renderer_, texture, nullptr, &rect);
+}
+
+void DemoState::DrawInstructions() {
+  SDL_Rect rect = { 0, 0, 0, 0 };
+  SDL_QueryTexture(instructions_texture_, nullptr, nullptr, &rect.w, &rect.h);
+  SDL_RenderCopy(renderer_, instructions_texture_, nullptr, &rect);
+}
+
+void DemoState::DrawIcons() {
+  for (size_t i = 0; i < channel_icons_.size(); ++i) {
+    DrawIcon(channel_icons_[i], channel_texture_);
+  }
+  for (size_t i = 0; i < listener_icons_.size(); ++i) {
+    DrawIcon(listener_icons_[i], listener_texture_);
+  }
+}
+
+bool RectContains(const SDL_Rect& rect, const Vector2f& point) {
+  return point.x() >= rect.x && point.x() < rect.x + rect.w &&
+         point.y() >= rect.y && point.y() < rect.y + rect.h;
+}
+
+void DemoState::HandleInput() {
+  SDL_Event event;
+  while (SDL_PollEvent(&event) != 0) {
+    switch (event.type) {
+      case SDL_QUIT: {
+        quit_ = true;
+        break;
+      }
+      case SDL_MOUSEBUTTONDOWN: {
+        Vector2f mouse_location(event.button.x, event.button.y);
+        if (event.button.button == SDL_BUTTON_LEFT) {
+          new_channel_location_ = mouse_location;
+        } else {
+          new_listener_location_ = mouse_location;
+        }
+        break;
+      }
+      case SDL_MOUSEBUTTONUP: {
+        Vector2f mouse_location(event.button.x, event.button.y);
+        SDL_Texture* texture;
+
+        texture = channel_texture_;
+        auto channel_iter =
+            find_if(channel_icons_.begin(), channel_icons_.end(),
+                    [mouse_location, texture](const ChannelIcon& icon) {
+                      SDL_Rect rect;
+                      TextureRect(&rect, icon.location, texture);
+                      return RectContains(rect, mouse_location);
+                    });
+        if (channel_iter != channel_icons_.end()) {
+          channel_iter->channel.Stop();
+          channel_icons_.erase(channel_iter);
+          break;
+        }
+
+        texture = listener_texture_;
+        auto listener_iter =
+            find_if(listener_icons_.begin(), listener_icons_.end(),
+                    [mouse_location, texture](const ListenerIcon& icon) {
+                      SDL_Rect rect;
+                      TextureRect(&rect, icon.location, texture);
+                      return RectContains(rect, mouse_location);
+                    });
+        if (listener_iter != listener_icons_.end()) {
+          audio_engine_.RemoveListener(&listener_iter->listener);
+          listener_icons_.erase(listener_iter);
+          break;
+        }
+
+        if (event.button.button == SDL_BUTTON_LEFT) {
+          channel_icons_.push_back(ChannelIcon());
+          ChannelIcon& icon = channel_icons_.back();
+          icon.location = new_channel_location_;
+          icon.velocity = mouse_location - new_channel_location_;
+          icon.channel = audio_engine_.PlaySound(sound_handle_);
+          break;
+        }
+
+        if (event.button.button == SDL_BUTTON_RIGHT) {
+          pindrop::Listener listener = audio_engine_.AddListener();
+          if (listener.Valid()) {
+            listener_icons_.push_back(ListenerIcon());
+            ListenerIcon& icon = listener_icons_.back();
+            icon.location = new_listener_location_;
+            icon.velocity = mouse_location - new_listener_location_;
+            icon.listener = listener;
+          }
+          break;
+        }
+        break;
+      }
+      default:
+        ;  // Do nothing.
+    }
+  }
+}
+
+void DemoState::AdvanceFrame(float delta_time) {
+  HandleInput();
+  UpdateIcons(delta_time);
+  audio_engine_.AdvanceFrame(delta_time);
+  SDL_RenderClear(renderer_);
+  DrawInstructions();
+  DrawIcons();
+  SDL_RenderPresent(renderer_);
+  SDL_Delay(kDelayMilliseconds);
+}
+
+void DemoState::Run() {
+  Uint32 previous_time = 0.0f;
+  Uint32 time = 0.0f;
+  while (!quit_) {
+    previous_time = time;
+    time = SDL_GetTicks();
+    float delta_time = (time - previous_time) / 1000.0f;
+    AdvanceFrame(delta_time);
+  }
+}
 
 int main(int argc, char* argv[]) {
   (void)argc;
   (void)argv;
-  pindrop::AudioEngine audio_engine;
-  std::cout << "Hello, world!" << std::endl;
+  DemoState demo;
+  if (!demo.Initialize()) {
+    fprintf(stderr, "Failed to initialize!\n");
+    return 1;
+  }
+  demo.Run();
   return 0;
 }
+

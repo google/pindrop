68c464abe2400693d7b4b158cffc447b198626d6
Alex Ames <amablue@google.com>
2015-04-27 09:46:36 -0700

    Update docs to b70c0b6
    b70c0b6 Updating all the documentation.

--- classpindrop_1_1_listener.js
+++ classpindrop_1_1_listener.js
@@ -4,7 +4,10 @@ var classpindrop_1_1_listener =
     [ "Listener", "classpindrop_1_1_listener.html#a25e3029605d34ad86124dfbe4e5d7507", null ],
     [ "Clear", "classpindrop_1_1_listener.html#ab1b15d517eb3e447af59c6662860002c", null ],
     [ "Location", "classpindrop_1_1_listener.html#ac7f894e1850e00c1e6867cbef95eaa39", null ],
+    [ "Matrix", "classpindrop_1_1_listener.html#a7f4a52a13c50161d2b88a3dc5a14b92e", null ],
     [ "SetLocation", "classpindrop_1_1_listener.html#a8e786627bd5247e758cd39840f9d3af9", null ],
+    [ "SetMatrix", "classpindrop_1_1_listener.html#a70c4d9228de17cada94791217cbb26d2", null ],
+    [ "SetOrientation", "classpindrop_1_1_listener.html#ae967db95fb6c6bbb25432636d9a56d40", null ],
     [ "state", "classpindrop_1_1_listener.html#a14f1de5b805602e0cc0f3cb673d31e4d", null ],
     [ "Valid", "classpindrop_1_1_listener.html#a6b3544c826a33714f31081735875aa95", null ]
 ];
--- search/all_6.js
+++ search/all_6.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['master_5fgain',['master_gain',['../classpindrop_1_1_audio_engine.html#ab3b64ede34e428fea39392627b19409b',1,'pindrop::AudioEngine']]],
+  ['matrix',['Matrix',['../classpindrop_1_1_listener.html#a7f4a52a13c50161d2b88a3dc5a14b92e',1,'pindrop::Listener']]],
   ['mute',['mute',['../classpindrop_1_1_audio_engine.html#a370e972f08393aa0fa0ff146cc6c9b61',1,'pindrop::AudioEngine']]],
   ['modifying_20assets',['Modifying Assets',['../pindrop_guide_assets.html',1,'']]]
 ];

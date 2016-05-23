e19a6322edadcdb4446f312f4568a4931ea16d1c
Stewart Miles <smiles@google.com>
2016-05-23 16:27:45 -0700

    Removed reference to deprecated NDK toolchain version.
    Change-Id: I6f7cd30a65d196fe004aed2b111938b1728f4814

--- jni/Application.mk
+++ jni/Application.mk
@@ -15,7 +15,7 @@
 APP_MODULES:=pindrop
 APP_STL:=gnustl_static
 APP_ABI:=armeabi armeabi-v7a-hard mips x86 x86_64
-NDK_TOOLCHAIN_VERSION := 4.8
+
 APP_CPPFLAGS += -std=c++11 -Wno-literal-suffix
 
 APP_PLATFORM := android-15
